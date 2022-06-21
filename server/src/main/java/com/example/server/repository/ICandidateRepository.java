3b2851c497245a66b1bbce4270efd1bf73ef4a31
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-06-21 20:37:09 +0200

    define candidate repository

--- server/src/main/java/com/example/server/repository/ICandidateRepository.java
+++ server/src/main/java/com/example/server/repository/ICandidateRepository.java
@@ -1,2 +1,8 @@
-package com.example.server.repository;public class ICandidateRepository {
+package com.example.server.repository;
+
+import com.example.server.models.Candidate;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ICandidateRepository extends JpaRepository<Candidate, Integer> {
+
 }
