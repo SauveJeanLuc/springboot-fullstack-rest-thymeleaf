56aca8693a107c4cbb70b5bb4e2cb002481cc5cd
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-06-21 20:37:43 +0200

    defined methods for service

--- server/src/main/java/com/example/server/services/ICandidateService.java
+++ server/src/main/java/com/example/server/services/ICandidateService.java
@@ -1,2 +1,23 @@
-package com.example.server.services;public class ICandidateService {
+package com.example.server.services;
+
+import com.example.server.dto.CreateOrUpdateCandidateDTO;
+import com.example.server.models.Candidate;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ICandidateService {
+
+    Optional<Candidate> getById(Integer id);
+
+    List<Candidate> getAll();
+
+    Candidate create(CreateOrUpdateCandidateDTO dto);
+
+    Candidate updateCandidate(Integer id, CreateOrUpdateCandidateDTO dto);
+
+    void delete(Integer id);
+
+    void vote(Integer id);
+
 }
