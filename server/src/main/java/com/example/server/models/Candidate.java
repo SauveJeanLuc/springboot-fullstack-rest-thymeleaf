a5e72221d345f378acc25db92102f2b72390b551
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-06-21 20:36:58 +0200

    define candidate model

--- server/src/main/java/com/example/server/models/Candidate.java
+++ server/src/main/java/com/example/server/models/Candidate.java
@@ -1,4 +1,35 @@
 package com.example.server.models;
 
+import com.example.server.dto.CreateOrUpdateCandidateDTO;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "candidates")
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
 public class Candidate {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Integer id;
+
+    private String name;
+
+    private String profileUrl;
+
+    private Integer votes;
+
+    public Candidate(CreateOrUpdateCandidateDTO dto) {
+        this.name = dto.getName();
+        this.profileUrl = dto.getProfileUrl();
+        this.votes = 0;
+    }
 }
