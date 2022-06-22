0df7700effa66162aee3619ee9958dd91cc8c456
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-06-22 12:40:36 +0200

    candidate model

--- client/src/main/java/com/example/client/model/Candidate.java
+++ client/src/main/java/com/example/client/model/Candidate.java
@@ -1,4 +1,21 @@
 package com.example.client.model;
 
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
 public class Candidate {
+
+    private Integer id;
+
+    private String name;
+
+    private String profileUrl;
+
+    private Integer votes;
+
 }
